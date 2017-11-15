3b140c964f1c3bfd2cd6d6ea2871e4fc08bc75b0
Berstanio <profiplayer2000@gmail.com>
2017-11-15 20:03:53 +0100

    Player Syncronizieren auf Basis von Events!

--- src/de/synchronizer/berstanio/Main.java
+++ src/de/synchronizer/berstanio/Main.java
@@ -2,13 +2,18 @@ package de.synchronizer.berstanio;
 
 import de.berstanio.com.core.Creator;
 import de.berstanio.com.handling.Utils;
+import net.minecraft.server.v1_8_R3.WorldSettings;
 import org.apache.commons.codec.digest.Md5Crypt;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerAnimationEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -42,7 +47,6 @@ public class Main extends JavaPlugin implements Listener{
                     identificator.setPassWord(Md5Crypt.md5Crypt(args[3].getBytes()));
                     identificator.setPassWord(args[4]);
                     Utils.sendeMessageAnServer(identificator);
-                    // TODO: 15.11.17 Eigene Daten senden
                 }else {
                     sender.sendMessage(ChatColor.RED + "Bitte geben sie die IP vom Server, einen Port, den Username, das Passwort und einen Oberbegriff, mit dem man diesen Server zu einer Gruppe zuorden kann, mit der er Synchronisiert wird an!");
                 }
@@ -52,6 +56,28 @@ public class Main extends JavaPlugin implements Listener{
         }
         return true;
     }
+
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event){//2 Shchaden 3 tot
+        Player player = event.getPlayer();
+        new MyPlayer(player.getName(), player.getLocation(),null, (float) player.getHealth(), WorldSettings.EnumGamemode.NOT_SET, 10, 10);
+    }
+
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event){//2 Shchaden 3 tot
+        assert event.getEntity() instanceof Player;
+        Player player = (Player) event.getEntity();
+        new MyPlayer(player.getName(), player.getLocation(),null, (float) player.getHealth(), WorldSettings.EnumGamemode.NOT_SET, 1, 2);
+    }
+
+    @EventHandler
+    public void onAnimation(PlayerAnimationEvent event){//2 Shchaden 3 tot
+        Player player = event.getPlayer();
+        new MyPlayer(player.getName(), player.getLocation(),null, (float) player.getHealth(), WorldSettings.EnumGamemode.NOT_SET, 0, 10);
+    }
+
 
     public ArrayList<SyncronizedPlayer> getSyncronizedPlayers() {
         return syncronizedPlayers;
